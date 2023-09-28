bd60a552796ad4be3bf2ce0515942de611f5ece2
Anirban <anirban21das@gmail.com>
2023-09-28 16:37:14 +0530

    my way

--- react-in-depth/src/App.jsx
+++ react-in-depth/src/App.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+// import './App.css';
 
-function App() {
+function Clock () {
+
+  const [ time, setTime ] = useState( new Date() );
+
+  useEffect( () => {
+    const newTime = new Date();
+    setTime( newTime );
+    if ( newTime.getSeconds() === 0 )
+    {
+      console.log( 'End of the minute' );
+    }
+  }, [ time ] );
+
+  // Format the time as HH:MM:SS
+  const formattedTime = time.toLocaleTimeString( [], { hour: '2-digit', minute: '2-digit', second: '2-digit' } );
+
   return (
-    <div>App</div>
-  )
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <h1 className="text-center">{ formattedTime }</h1>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+function App () {
+  return (
+    <div className="App">
+      <Clock />
+    </div>
+  );
+}
+
+export default App;
